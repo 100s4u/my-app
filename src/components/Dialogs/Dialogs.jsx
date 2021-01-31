9c5a1b004941f0306c349f775a33a89911cf4a3f
Sandman <sandman.4ever@mail.ru>
2021-01-31 16:55:58 +0300

    Added styles for Dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -5,7 +5,19 @@ const Dialogs = () =>{
 			<main>
 				<div className="row">
 					<div className={classes.main__contnet}>
-						<div>text</div>
+						<div className={classes.main__contnet__dialogs}>
+							<div className={classes.dialogs_items}>
+								<div className={classes.dialog}>Sas</div>
+								<div className={classes.dialog}>Alisa</div>
+								<div className={classes.dialog}>As</div>
+								<div className={classes.dialog}>Sos</div>
+							</div>
+							<div className={classes.messages}>
+								<div className={classes.message}>Test</div>
+								<div className={classes.message}>Testing</div>
+								<div className={classes.message}>The Test</div>
+							</div>
+						</div>
 					</div>
 				</div>
 			</main>	
