4f3fc8e33839566c681c06b1f86138ae3ebf313e
100s4u <sandman.4ever@mail.ru>
2021-03-21 23:34:16 +0300

    Added ref's and simple onClick func

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,5 +1,6 @@
 import classes from './Dialogs.module.css';
 import {NavLink} from 'react-router-dom';
+import React from 'react';
 
 const Dialogs = (props) => {
 
@@ -17,6 +18,12 @@ const Dialogs = (props) => {
 	let messagesElements = props.state.messages.map(m => <Message message={m.message} className={classes.message} />)
 	let dialogsElement = props.state.dialogs.map(el => <DialogItem id={el.id} name={el.name} />)
 
+	let newMessageElement = React.createRef();
+	let addMessage = () => {
+		let text = newMessageElement.current.value;
+		alert(text);
+	}
+
 	return (
 			<main>
 				<div className="row">
@@ -25,8 +32,14 @@ const Dialogs = (props) => {
 							<div className={classes.dialogs_items}>
 								{dialogsElement}
 							</div>
-							<div className={classes.messages}>
-								{messagesElements}
+							<div className={classes.messages__block}>	
+								<div className={classes.messages}>
+									{messagesElements}
+								</div>
+								<div className={classes.message__input}>
+								<textarea ref={ newMessageElement }></textarea>
+									<button onClick={ addMessage }>Send</button>
+								</div>
 							</div>
 						</div>
 					</div>
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,13 +1,21 @@
+import React from 'react';
 import classes from './MyPosts.module.css';
 import Post from './Post/Post'
 
+let newPostElement = React.createRef();
+
+let addPost = () => {
+	let text = newPostElement.current.value;
+	alert(text);
+}
 
 const MyPosts = (props) => {
 	let postsElement = props.posts.map(p => <Post id={p.id} text={p.message} likesCount={p.likesCount} />)
 	return (
 			<div className={classes.main__contnet__block}>
 				<div className={classes.add__form}>
-					<input type="text" />
+					<button onClick={ addPost }>Add post</button>
+					<input type="text" ref={ newPostElement }/>
 				</div>
 				{postsElement}
 			</div>
